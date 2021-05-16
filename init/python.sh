3db42d10dc5d8296ba2e7a7be7107a9a3c83c958
Rommert Zijlstra <rommert.thomas@gmail.com>
2021-05-17 01:33:37 +0200

    overhaul pt2

--- init/python.sh
+++ init/python.sh
@@ -3,6 +3,8 @@
 # update path for this session
 export PATH="$HOME/.local/bin:/usr/local/opt/python@3.9/libexec/bin:$PATH"
 
+pip install --upgrade pip setuptools wheel
+
 # install pipx first...
 pip install pipx
 pip install mccabe
@@ -14,6 +16,6 @@ pipx install black
 pipx install flake8
 pipx install isort
 pipx install mypy
-pipx install pipenv
+pipx install poetry
 pipx install pyflakes
 pipx install python-language-server
