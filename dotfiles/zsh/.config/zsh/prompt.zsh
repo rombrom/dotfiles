88bca6682160fff4e4cc6b9e6b50437e032ba453
Rommert Zijlstra <rommert.thomas@gmail.com>
2021-05-17 02:07:56 +0200

    chore: cleanup

--- dotfiles/zsh/.config/zsh/prompt.zsh
+++ dotfiles/zsh/.config/zsh/prompt.zsh
@@ -7,7 +7,7 @@ zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' stagedstr ' ⌥'
 zstyle ':vcs_info:*' unstagedstr ' +'
-zstyle ':vcs_info:*' formats '%F{1}%b%f%F{3}%c%u%f '
+zstyle ':vcs_info:*' formats '%F{12}%b%f%F{3}%c%u%f '
 
 add-zsh-hook precmd vcs_info
 
