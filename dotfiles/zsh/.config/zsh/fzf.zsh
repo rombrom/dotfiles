73df07bc242324ed763a21457e8a2c149e8804a2
Rommert Zijlstra <rommert.thomas@gmail.com>
2022-02-11 22:00:23 +0100

    WHERE DO MY EYES NEED TO GO!?

--- dotfiles/zsh/.config/zsh/fzf.zsh
+++ dotfiles/zsh/.config/zsh/fzf.zsh
@@ -5,7 +5,7 @@ fi
 
 # Make fzf use ripgrep by default
 export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
-export FZF_DEFAULT_OPTS="--height 40% --tiebreak=length,end \
+export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --tiebreak=length,end \
   --color='16,bg+:-1,fg+:4,gutter:-1,hl:3,hl+:3,pointer:4,prompt:6' \
   --preview='bat {}'"
 
