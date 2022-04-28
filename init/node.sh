62b92c66cb75655e1ecd30c63fd4f3c1daba28fc
Rommert Zijlstra <rommert.thomas@gmail.com>
2022-04-28 10:28:07 +0200

    Update nx cli global installatio

--- init/node.sh
+++ init/node.sh
@@ -5,11 +5,11 @@ export PATH="/usr/local/opt/node@14/bin:$PATH"
 
 # Update NPM to the latest version
 npm install -g \
-  @nrwl/cli@latest \
   eslint@latest \
   eslint-config-prettier@latest \
   eslint-plugin-prettier@latest \
   eslint_d@latest \
+  nx \
   prettier@latest \
   stylelint@latest \
   stylelint-config-standard@latest \
