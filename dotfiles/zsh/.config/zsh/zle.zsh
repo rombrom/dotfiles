91dcf743e93d2be86144f7f08edb2e8b163a21be
Rommert Zijlstra <rommert.thomas@gmail.com>
2023-02-01 16:13:41 +0100

    C-Z! I just think it's neat!

--- dotfiles/zsh/.config/zsh/zle.zsh
+++ dotfiles/zsh/.config/zsh/zle.zsh
@@ -10,10 +10,8 @@ bindkey ' ' magic-space # [Space] - don't do history expansion
 
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
-
 bindkey '\C-p' up-line-or-beginning-search
 bindkey '\C-n' down-line-or-beginning-search
-
 bindkey "^[[A" up-line-or-beginning-search
 bindkey "^[[B" down-line-or-beginning-search
 
@@ -21,3 +19,19 @@ zle -N edit-command-line
 bindkey '\C-x\C-e' edit-command-line
 
 bindkey '\C-x\C-i' _complete_help
+
+# Shamelessly borrowed from
+# https://sheerun.net/2014/03/21/how-to-boost-your-vim-productivity/
+# 🙏
+function __czcz() {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line -w
+  else
+    zle push-input -w
+    zle clear-screen -w
+  fi
+}
+
+zle -N __czcz
+bindkey '\C-z' __czcz
