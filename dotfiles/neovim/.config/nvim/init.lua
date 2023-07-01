52ec235b6f7ff474f29ace56e223234d167edd8a
Rommert Zijlstra <rommert.thomas@gmail.com>
2023-07-01 21:56:29 +0200

    Look ma! I've migrated to a Lua plugin system

--- dotfiles/neovim/.config/nvim/init.lua
+++ dotfiles/neovim/.config/nvim/init.lua
@@ -1,3 +1,10 @@
--- Yeah...
-vim.o.runtimepath = vim.env.HOME .. '/.vim,' .. vim.o.runtimepath .. ',' .. vim.env.HOME .. '/.vim/after'
+require('plugins');
+
+-- -- Yeah...
+vim.opt.runtimepath:prepend({
+  vim.env.HOME .. '/.vim',
+  vim.o.runtimepath,
+  vim.env.HOME .. '/.vim/after',
+})
+
 vim.cmd('source ' .. vim.env.HOME .. '/.vim/vimrc')
