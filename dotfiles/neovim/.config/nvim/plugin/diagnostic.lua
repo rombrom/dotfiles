f5d9f64150235d84b9636124df8bd263d5ba8e03
Rommert Zijlstra <rommert.thomas@gmail.com>
2023-07-22 20:44:07 +0200

    Diagnostics

---
+++ dotfiles/neovim/.config/nvim/plugin/diagnostic.lua
@@ -0,0 +1,6 @@
+vim.fn.sign_define({
+  { name = "DiagnosticSignError", text = "‼", texthl = "DiagnosticSignError" },
+  { name = "DiagnosticSignHint", text = "⏿", texthl = "DiagnosticSignHint" },
+  { name = "DiagnosticSignInfo", text = "ℹ", texthl = "DiagnosticSignInfo" },
+  { name = "DiagnosticSignWarn", text = "!", texthl = "DiagnosticSignWarn" },
+})
