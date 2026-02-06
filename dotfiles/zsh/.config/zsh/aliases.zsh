042bb91b9e369783064ac6c4165c5b37b4245f59
Rommert Zijlstra <rommert.thomas@gmail.com>
2026-02-06 15:06:01 +0100

    chore: some llm aliases

--- dotfiles/zsh/.config/zsh/aliases.zsh
+++ dotfiles/zsh/.config/zsh/aliases.zsh
@@ -55,3 +55,60 @@ fi
 
 # Reload zshrc
 alias reload='clear && exec zsh'
+
+# llms
+alias llm-qwen-vl='llama-server \
+  --host 0.0.0.0 \
+  --port 8484 \
+  -hf unsloth/Qwen3-VL-30B-A3B-Instruct-GGUF:UD-Q4_K_XL \
+  --n-gpu-layers 99 \
+  --jinja \
+  --top-p 0.8 \
+  --top-k 20 \
+  --temp 0.7 \
+  --min-p 0.0 \
+  --flash-attn on \
+  --presence-penalty 1.5 \
+  --ctx-size 131071'
+alias llm-qwen-coder='llama-server \
+  --host 0.0.0.0 \
+  --port 8484 \
+  -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:UD-Q4_K_XL \
+  --n-gpu-layers 99 \
+  --jinja \
+  --top-p 0.8 \
+  --top-k 20 \
+  --temp 0.7 \
+  --min-p 0.0 \
+  --repeat-penalty 1.05 \
+  --ctx-size 131071'
+alias llm-qwen-coder-next='llama-server \
+  --host 0.0.0.0 \
+  --port 8484 \
+  -hf unsloth/Qwen3-Coder-Next-GGUF:UD-Q2_K_XL \
+  --jinja \
+  --top-p 0.95 \
+  --top-k 40 \
+  --temp 1.0 \
+  --min-p 0.01 \
+  --fit on'
+alias llm-glm='llama-server \
+  --host 0.0.0.0 \
+  --port 8484 \
+  -hf unsloth/GLM-4.7-Flash-GGUF:UD-Q4_K_XL \
+  --jinja \
+  --threads -1 \
+  --ctx-size 131071 \
+  --temp 0.7 \
+  --top-p 1.0 \
+  --min-p 0.01 \
+  --fit on'
+alias llm-devstral='llama-server \
+  --host 0.0.0.0 \
+  --port 8484 \
+  -hf unsloth/Devstral-Small-2-24B-Instruct-2512-GGUF:UD-Q4_K_XL \
+  --jinja \
+  -ngl 99 \
+  --threads -1 \
+  --ctx-size 131071 \
+  --temp 0.15'
